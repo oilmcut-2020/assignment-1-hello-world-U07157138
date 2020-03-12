f9e65c390dd8214363d2f54f0472aa743a70c78f
U07157138 <49745864+U07157138@users.noreply.github.com>
2020-03-12 20:09:38 +0800

    Update U07157138.java

--- U07157138.java
+++ U07157138.java
@@ -6,7 +6,7 @@ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, S
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-public class Main
+public class U07157138
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
